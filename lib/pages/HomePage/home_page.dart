bca29267b5db5e6545a2713bf7ab1fd2a3baf137
Niks Kozlovs <niks.kozlovs@chi.lv>
2023-01-06 15:44:01 +0200

    Added pageview to home page

--- lib/pages/HomePage/home_page.dart
+++ lib/pages/HomePage/home_page.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:mediamanager_flutter/pages/HomePage/MoviesPage/movies_page.dart';
+import 'package:mediamanager_flutter/pages/HomePage/TVShowsPage/tv_shows_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _pageController = PageController();
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: const MoviesPage(),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (int index) {
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.movie_outlined),
+            label: 'Movies',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.tv),
+            label: 'TV Shows',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: const [
+          MoviesPage(),
+          TVShowsPage(),
+        ],
+      ),
     );
   }
 }
